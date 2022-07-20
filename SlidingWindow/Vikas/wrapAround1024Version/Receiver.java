c03309b4a849f5fead5e50abf589cfcce355a055
Vikas Shenoy Pete <89056652+vikas-shenoy-p@users.noreply.github.com>
2022-07-19 22:31:40 -0700

    Add files via upload

---
+++ SlidingWindow/Vikas/wrapAround1024Version/Receiver.java
@@ -0,0 +1,118 @@
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//Server
+public class Receiver {
+
+        ServerSocket receiver;
+        Socket conc = null;
+
+        ObjectOutputStream out;
+        ObjectInputStream in;
+
+        String ack, pkt= "";
+        int count = 0;
+        int winSize = 0;
+        int loopCount = 0;
+        int seqNum = 1;
+        boolean flag = false;
+
+        private int getPrevSeqNum(){
+            return seqNum;
+        }
+
+//        private int getNextSeqNum(){
+//
+//        }
+
+        public void run() throws IOException, InterruptedException, ClassNotFoundException {
+            receiver = new ServerSocket(1500, 10);
+            conc = receiver.accept();
+
+            if (conc != null)
+                System.out.println("Connection established");
+
+            out = new ObjectOutputStream(conc.getOutputStream());
+            in = new ObjectInputStream(conc.getInputStream());
+
+            do {
+                //make up for packet loss
+                if(flag){
+                    try{
+                    String lostAck = (String) in.readObject();
+                    System.out.println("\nlost message received : " + lostAck);
+                    //seqNum++;
+                    flag = false;
+                }
+                catch(Exception e){
+                }
+                }
+                
+                else 
+                {
+                    //Get sliding window size
+                    try {
+                        String size = (String) in.readObject();
+                        winSize = Integer.parseInt(size);
+                        System.out.println("---------------------------------");
+                        System.out.println("\nWindow size received : " + winSize);
+                    } catch (Exception e) {
+                    }
+
+                    loopCount = 0;
+
+                    while (loopCount < winSize) {
+                        try {
+                            pkt = (String) in.readObject();
+                            ack = pkt;
+                            System.out.println("\nMsg received : " + ack);
+                            count = Integer.parseInt(ack);
+                            if (seqNum>=Math.pow(2,16)+1){
+                                count=1;
+                                seqNum=1;
+                            }
+                            if (count != getPrevSeqNum()) {
+                                //lost packet
+                                System.out.println("Lost packet!");
+                                flag = true;
+                                sendACK(getPrevSeqNum());
+                            } else {
+                                // sendACK(count + 1);
+                                sendACK(count + 1024);
+                            }
+                            // seqNum = count + 1;
+                            seqNum = count + 1024;
+                            loopCount++;
+                        } catch (EOFException e) {
+            //   System.out.println("End");
+                        }
+                    }
+                }
+            }
+            while(winSize <= Math.pow(2,16)+1);
+
+            in.close();
+            out.close();
+            receiver.close();
+            System.out.println("\nConnection Terminated.");
+        }
+
+        //to send acknowledgement to client
+        private void sendACK(int count){
+            try {
+                ack = Integer.toString(count);
+                out.writeObject(ack);
+                out.flush();
+                System.out.println("Sending Ack " + count);
+            }catch(Exception e){
+            }
+        }
+        public static void main(String args[]) throws IOException, InterruptedException, ClassNotFoundException {
+           Receiver R = new Receiver();
+           R.run();
+        }
+}
---
+++ SlidingWindow/Vikas/wrapAround1024Version/Sender.java
@@ -0,0 +1,144 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+//Client
+public class Sender {
+
+        Socket sender;
+
+        ObjectOutputStream out;
+        ObjectInputStream in;
+
+        String pkt;
+        int SeqNum = 1;
+        int ackNum = 0;
+        int missingSeq = 0;
+        int slideWin = 1;
+        boolean flag = false;
+        int SegNum=1;
+        int rando=0;
+
+        void sendFrameSize() throws IOException {
+            String win = String.valueOf(slideWin);
+            out.writeObject(win);
+            System.out.println("Window size  " + slideWin);
+            out.flush();
+        }
+        void generateRandom(){
+            rando = (int)(Math.random() * (Math.pow(2,16) + 1)); //generate number between 1 and 2^16
+            rando=(rando*1024)+1; //convert number to a multiple of (1024+1)
+        }
+
+        public void sendFrames() throws IOException {
+            generateRandom();
+            System.out.println(rando);
+            if(SeqNum == rando){
+                 SeqNum+=1024;
+            //     // SeqNum+=1;
+              }
+            pkt = String.valueOf(SeqNum);
+            out.writeObject(pkt);
+            System.out.println("Sent  " + SeqNum);
+            System.out.println("Seg" + SegNum);
+            out.flush();
+            SeqNum=SeqNum+1024;
+            // SeqNum++;
+            SegNum++;
+        }
+        
+
+        public void sendLostFrame(int lostSeq) throws IOException {
+
+            System.out.println("-----sending lost packet!-------------");
+            pkt = String.valueOf(lostSeq);
+            out.writeObject(pkt);
+            int lost = Integer.parseInt(pkt);
+            System.out.println("Sent  " + lost);
+            out.flush();
+            flag = false;
+            slideWin +=1;
+            SegNum++;
+        }
+
+        //calculate the next sequence number
+        int getNextSeqNumber(){
+            return SeqNum;
+        }
+
+        //calculate the previous sequence number to verify ack received
+        int getPrevSeqNumber(){
+            // return --SeqNum;
+            return SeqNum - 1024;
+        }
+
+
+        public void run() throws IOException, ClassNotFoundException {
+            sender = new Socket("localhost", 1500);
+
+            //sender = new Socket("192.168.103.124", 1500);
+            out = new ObjectOutputStream(sender.getOutputStream());
+            in = new ObjectInputStream(sender.getInputStream());
+            while ((slideWin <= Math.pow(2,16)+1) && SegNum<=100000){ //need to set this limit to 10Million
+                    
+                    System.out.println("---------------------------------");
+                    sendFrameSize();
+
+                    int loopCount=0;
+                    while ((loopCount < slideWin) && SegNum<=100000) { //need to set this limit to 10Million
+                        if (SeqNum>=Math.pow(2,16)+1){
+                            SeqNum=1;
+                        }
+                        sendFrames();
+                        receiveACK();
+                        detectPacketLoss();
+                        loopCount++;
+                    }
+                    //Modify the sliding window
+                    //if (SeqNum == ackNum) {
+                    if((!flag)){
+                        if ((slideWin*2)<=Math.pow(2,16)+1){slideWin *= 2; }
+                        else {continue;}
+                        
+                    }
+                    else{
+                        //next packet to be sent is packet with the received ACK no
+                        //flag = false;
+                        //missingSeq = ackNum;
+                        if(slideWin > 1)
+                            slideWin = slideWin / 2;
+                        sendLostFrame(missingSeq);
+                    }
+                }   
+            in.close();
+            out.close();
+            sender.close();
+            System.out.println("\nConnection Terminated");
+        }
+
+        private void detectPacketLoss() {
+            //return SeqNum > ackNum ? true: false;
+            if(SeqNum > ackNum){
+                if(!flag) {
+                    missingSeq = ackNum;
+                    flag = true;
+                }
+            }
+
+        }
+
+        private void receiveACK() {
+            try {
+                String Ack = (String) in.readObject();
+                ackNum = Integer.parseInt(Ack);
+                System.out.println("ACK received : " + ackNum);
+            }catch (Exception e) {
+            }
+        }
+
+        public static void main(String args[]) throws IOException, ClassNotFoundException {
+            Sender s = new Sender();
+            s.run();
+        }
+}
